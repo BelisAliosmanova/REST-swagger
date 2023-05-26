76f1b0d7d670fac8ce643049b1f8194dc8a058b9
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-05-26 20:25:23 +0300

    yaml

--- src/test/java/com/example/demo/HomeworkTema9ApplicationTests.java
+++ src/test/java/com/example/demo/HomeworkTema9ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HomeworkTema9ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
